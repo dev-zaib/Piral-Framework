7549ddb7c029b3759426ce5185a1698b9d0caeef
mughira <mughira3107@gmail.com>
2024-01-24 12:55:20 +0500

    Add Newly Generated API Link

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,7 +4,7 @@ import { createInstance, Piral, createStandardApi } from 'piral';
 import { layout, errors } from './layout';
 
 // change to your feed URL here (either using feed.piral.cloud or your own service)
-const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/empty';
+const feedUrl = 'https://feed.piral.cloud/api/v1/pilet/dummy-feeds';
 
 const instance = createInstance({
   state: {
